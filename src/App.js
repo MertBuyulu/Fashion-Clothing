fb6f569cdcf21cda2ae470a424b068b84f7e6868
Mert Buyulu <62453255+MertBuyulu@users.noreply.github.com>
2021-11-27 18:05:05 -0600

    integrate initial routing from the landing page to other pages using react-router-dom v6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/shop/hats" element={<Hats />} />
+    </Routes>
   );
 }
+
+const Hats = (props) => {
+  console.log(props);
+  return <h1>WELCOME TO THE HATS PAGE</h1>;
+};
 
 export default App;
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./menu-item.styles.scss";
+import { useNavigate } from "react-router-dom";
+// useParams hook
+// useLocation hook
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  const navigate = useNavigate();
+  console.log(useNavigate(linkUrl));
   return (
-    <div className={`${size} menu-item`}>
+    <div className={`${size} menu-item`} onClick={() => navigate(`${linkUrl}`)}>
       <div
         className="background-image"
         style={{
