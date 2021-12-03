1b6fc486ffc3b85711ca66ddc7f8e7722f4cd3bb
Mert Buyulu <62453255+MertBuyulu@users.noreply.github.com>
2021-12-02 20:28:54 -0600

    create cart item component and connect it to cart dropdown menu for shopping cart preview

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,11 +3,21 @@ import React from "react";
 import "./cart-dropdown.styles.scss";
 // compoenents
 import CustomButton from "../custom-button/custom-button.component";
+import CartItem from "../cart-item/cart-item.component";
+//redux
+import { useSelector } from "react-redux";
 
 const CardDropDown = () => {
+  // get the items added to the cart in real time
+  const cartItems = useSelector((state) => state.cart.cartItems);
+
   return (
     <div className="card-container">
-      <div className="card-items"></div>
+      <div className="card-items">
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );
