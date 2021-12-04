c69b3366ef1b00ffad29a573f8b83a5f4da66923
Mert Buyulu <62453255+MertBuyulu@users.noreply.github.com>
2021-12-03 21:21:10 -0600

    add addItem and removeItem functionality to checkout item component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,17 +1,47 @@
 import React from "react";
 //styles
 import "./checkout-item.styles.scss";
+// redux
+import { useDispatch } from "react-redux";
+import {
+  clearItemFromCart,
+  addItem,
+  removeItem,
+} from "../../redux/cart/cart.actions";
 
-const CheckoutItem = ({ cartItem: { name, price, imageUrl, quantity } }) => {
+const CheckoutItem = ({ cartItem }) => {
+  const { id, name, price, imageUrl, quantity } = cartItem;
+  const dispatch = useDispatch();
   return (
     <div className="checkout-item">
       <div className="image-container">
         <img src={imageUrl} alt={name} />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <div className="quantity">
+        <div
+          className="arrow"
+          role="button"
+          onClick={() => dispatch(removeItem(cartItem))}
+        >
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div
+          className="arrow"
+          role="button"
+          onClick={() => dispatch(addItem(cartItem))}
+        >
+          &#10095;
+        </div>
+      </div>
       <span className="price">{price}</span>
-      <div className="remote-button">&#10005;</div>
+      <div
+        className="remote-button"
+        onClick={() => dispatch(clearItemFromCart(id))}
+      >
+        &#10005;
+      </div>
     </div>
   );
 };
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -9,7 +9,7 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   if (existingCartItem) {
     return cartItems.map((cartItem) =>
       cartItem.id === cartItemToAdd.id
-        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        ? { ...cartItem, quantity: ++cartItem.quantity }
         : cartItem
     );
   }
@@ -17,3 +17,14 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   // if the item does not exists in our cart, just append the new cart item to the end of cartItems array and return it.
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) =>
+  // if the ids match, check if the item has quantity of one or more, if one then remove it from the cart
+  // otherwise decrement its value by one.
+  cartItems.flatMap((cartItem) => {
+    return cartItem.id !== cartItemToRemove.id
+      ? cartItem
+      : cartItem.quantity === 1
+      ? []
+      : { ...cartItem, quantity: --cartItem.quantity };
+  });
