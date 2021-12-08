2af9b2a9c288a87817c1061501fcb16490c64394
Mert Buyulu <62453255+MertBuyulu@users.noreply.github.com>
2021-12-08 13:38:50 -0600

    update checkout page component with a button, which will be used to integrate stripe payment to the app once the backend server is ready

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -41,6 +41,12 @@ const CheckOutPage = () => {
       <div className="total">
         <span>TOTAL: ${total}</span>
       </div>
+      <div className="test-warning">
+        *Please use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+      </div>
+      <button>Pay Now</button>
     </div>
   );
 };
