ee2cd48809fd85683f699d3423e572c634e6a8e4
Mert Buyulu <62453255+MertBuyulu@users.noreply.github.com>
2021-12-14 16:36:10 -0600

    create client folder and put the existing project inside

--- src/components/custom-button/custom-button.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-// styled component
-import * as S from "./custom-button.styles";
-
-const CustomButton = ({ children, ...props }) => {
-  return <S.CustomButton {...props}>{children}</S.CustomButton>;
-};
-
-export default CustomButton;
--- src/pages/homepage/homepage.styles.jsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-// works exactly like css or scss
-export const HomePage = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
